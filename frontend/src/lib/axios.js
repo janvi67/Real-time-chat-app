dbc42a2a1813f5aa976092019d163597e2fb1cb4
janvi67 <janviraychura7890@gmail.com>
2025-05-06 17:50:49 +0530

    add vercel.json

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://real-time-chat-app-kcv7.vercel.app/api",
   withCredentials: true,
 });
