412213c69b747f29c09732f40a0a6dbbafcca5dc
janvi67 <janviraychura7890@gmail.com>
2025-05-06 19:47:13 +0530

    commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://real-time-chat-app-kcv7.vercel.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://real-time-chat-app-backend-9p1k.onrender.com/api",
   withCredentials: true,
 });
